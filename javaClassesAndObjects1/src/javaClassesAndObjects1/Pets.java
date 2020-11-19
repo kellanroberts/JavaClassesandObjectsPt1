9bece890521e92dd0f884894380f72577ff77332
kellanroberts <73409978+kellanroberts@users.noreply.github.com>
2020-11-18 19:40:29 -0500

    Add files via upload

---
+++ javaClassesAndObjects1/src/javaClassesAndObjects1/Pets.java
@@ -0,0 +1,54 @@
+package javaClassesAndObjects1;
+
+public class Pets<String> {
+	private String name;
+	private int age;
+	private String location;
+	private String type;
+	
+	public Pets(String name,
+			int age,
+			String location,
+			String type) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.location = location;
+		this.type = type;
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	// set name, age, location
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public void setLocation(String location) {
+		this.location = location; 
+	}
+}
+	
+	
+	
+	
+	
+	
+	
+	
---
+++ javaClassesAndObjects1/src/javaClassesAndObjects1/Pokemon.java
@@ -0,0 +1,70 @@
+package javaClassesAndObjects1;
+
+public class Pokemon<String> {
+	private String type;
+	private String name;
+	private boolean canEvolve;
+	private int hitPoints;
+	
+	public Pokemon(String type,
+			String name,
+			boolean canEvolve,
+			int hitPoints) {
+		super();
+		this.type = type;
+		this.name = name;
+		this.canEvolve = canEvolve;
+		this.hitPoints = hitPoints;
+		
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isCanEvolve() {
+		return canEvolve;
+	}
+
+	public void setCanEvolve(boolean canEvolve) {
+		this.canEvolve = canEvolve;
+	}
+
+	public int getHitPoints() {
+		return hitPoints;
+	}
+
+	public void setHitPoints(int hitPoints) {
+		this.hitPoints = hitPoints;
+	}
+
+
+	public static <String> void main(String[] args) {
+		
+		String Pikachu;
+		String name = Pikachu;
+		String Electric;
+		String type = Electric;
+		
+		System.out.prntln(name "is an" type  "pokemon!");
+	
+	
+	public static void main(boolean)
+	
+		if canEvolve = true
+	
+		
+	}
+
